d6258b34bdb582cd517a47bb616852c572e05889
Madhu-221 <115968087+Madhu-221@users.noreply.github.com>
2023-03-10 18:08:33 +0530

    staggers-function

--- js/function.js
+++ js/function.js
@@ -14,3 +14,43 @@ let tl = gsap.timeline({repeat: -1, repeatDelay: 1});
 tl.to(".green-rot", { rotation: '360' });
 tl.to(".purple-rot", { rotation: '360' });
 tl.to(".orange-rot", { rotation: '360' });
+
+
+// Stagger function
+
+gsap.from(".simple-staggers", {
+    duration: 2,
+    scale: 0.5, 
+    opacity: 0, 
+    delay: 0.5, 
+    stagger: 0.2,
+    ease: "elastic", 
+    force3D: true, 
+  });
+  
+  document.querySelectorAll(".simple-staggers").forEach(function(simpleStaggers) {
+    simpleStaggers.addEventListener("click", function() {
+      gsap.to(".simple-staggers", {
+        duration: 0.5, 
+        opacity: 0, 
+        y: -100, 
+        stagger: 0.1,
+        ease: "back.in"
+      });
+    });
+  });
+// 3d-Stagger
+  gsap.to(".d-stagger-box", 1, {
+    scale: 0.1, 
+    y: 60,
+    yoyo: true, 
+    repeat: -1, 
+    ease: "power1.inOut",
+    delay: 1,
+    stagger: {
+      amount: 1.5, 
+      grid: "auto",
+      from: "center"
+    }
+  });
+  
