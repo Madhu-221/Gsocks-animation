258775215eeab2e62977239d790047eea814a252
Madhu-221 <115968087+Madhu-221@users.noreply.github.com>
2023-03-14 18:05:50 +0530

    scroll_animation

--- js/function.js
+++ js/function.js
@@ -42,7 +42,7 @@ gsap.from(".simple-staggers", {
 // 3d-Stagger
   gsap.to(".d-stagger-box", 1, {
     scale: 0.1, 
-    y: 60,
+    // y: 60,
     yoyo: true, 
     repeat: -1, 
     ease: "power1.inOut",
@@ -54,3 +54,58 @@ gsap.from(".simple-staggers", {
     }
   });
   
+
+  // spiderMan section animation
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.to(".spiderman_logo", { scrollTrigger:{
+    tigger:".spiderman_logo",
+    start:"center  center",
+    toggleActions: "restart",
+  }, duration: 3,  x:"400", backgroundColor:"rgb(38 49 91)", borderRadius:"20%", border:"4px solid #fff", ease:"smooth", })
+
+
+// canvas
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "#28a92b";
+
+let position = { x: 0, y: 0 };
+
+function draw() {
+  // erase the canvas
+  ctx.clearRect(0, 0, 300, 300);
+  // redraw the square at it's new position
+  ctx.fillRect(position.x, position.y, 100, 100);
+}
+
+//animate x and y of point
+gsap.to(position, { 
+  x: 200, 
+  y: 200, 
+  duration: 4,
+  onUpdate: draw,
+  repeat: -1,
+});
+
+
+// control function
+
+// click handlers for controlling the tween instance...
+let nav = document.querySelector('.nav')
+
+let tween = gsap.to(".navy", {
+  duration: 4, 
+  x: () => nav.offsetWidth, // animate by the px width of the nav
+  xPercent: -100, // offset by the width of the box
+  rotation: 360, 
+  ease: "none", 
+  paused: true
+});
+
+// click handlers for controlling the tween instance...
+document.querySelector("#play").onclick = () => tween.play();
+document.querySelector("#pause").onclick = () => tween.pause();
+document.querySelector("#resume").onclick = () => tween.resume();
+document.querySelector("#reverse").onclick = () => tween.reverse();
+document.querySelector("#restart").onclick = () => tween.restart();
